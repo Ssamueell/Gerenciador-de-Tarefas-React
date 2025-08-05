cba17b74c939c41056941789dfc88259a581fa64
Samuel Werlang <samuelwerlang13@gmail.com>
2025-08-05 17:16:34 -0300

    start

---
+++ src/App.jsx
@@ -0,0 +1,34 @@
+import { useState } from "react";
+import "./App.css";
+import AddTask from "./components/AddTask";
+import Tasks from "./components/Tasks";
+
+function App() {
+  const [tasks, setTasks] = useState([
+    {
+      id: 1,
+      title: "Tarefa 1",
+      description: "Descrição da tarefa 1",
+      completed: false,
+    },
+    {
+      id: 2,
+      title: "Tarefa 2",
+      description: "Descrição da tarefa 2",
+      completed: true,
+    },
+  ]);
+  return (
+    <div className="w-screen h-screen bg-slate-500 flex justify-center p-6">
+      <div className=" w-[500px]">
+        <h1 className="text-3xl text-slate-100 font-bold text-center">
+          Gerenciador de tarefas
+        </h1>
+        <Tasks tasks={tasks} />
+        <AddTask />
+      </div>
+    </div>
+  );
+}
+
+export default App;
